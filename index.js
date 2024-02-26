565abccbf93b9d6986724a169fb0a167f25d8a94
Anthony Chung <chunga168@gmail.com>
2024-02-26 23:42:24 +0800

    added logs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import mysql from 'mysql2/promise';
+import winston from 'winston';
 
 import UserService from './services/user.js';
 const mongoDbServer = process.env.easyAIOpsDBUrl || 'host.docker.internal';
@@ -11,16 +12,45 @@ const service = new UserService();
 const app = express();
 const port = 3000;
 
+// https://www.npmjs.com/package/winston
+const { combine, timestamp, label, printf  } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'index.js' }),
+    timestamp(),
+    myFormat
+  ),
+  // defaultMeta: { service: 'index.js' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+
 let foreverLoop = false;
 let foreverRead = false;
 let count = 0;
+
+// https://stackify.com/node-js-logging/
+app.use(logRequest);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
+  logger.info(`App listening on port ${port}`);
 });
 
 // Connections
@@ -29,9 +59,10 @@ mongoose.set('strictQuery', true);
 mongoose.connect(DB_URL).
 then(() => {
   console.log(`MongoDB connection successful`); 
-
+  logger.info(`MongoDB connection successful`);
 }).catch((e) => {
   console.error('Error connecting to MongoDB: ', e);
+  logger.error('Error connecting to MongoDB: ', e);
 });
 
 let mySqlConnection;
@@ -45,10 +76,12 @@ try {
    });
    mySqlConnection.execute('SELECT COUNT(*) FROM \`easy-aiops\`.\`users\`').then(results => {
      // console.log(results);
-     console.log('MySql connection successful');  
+     console.log('MySql connection successful');
+     logger.info('MySql connection successful');
    });
 } catch (e) {
   console.error('Error connecting to MySQL: ', e);
+  logger.error('Error connecting to MySQL: ', e);
 }
 
 // APIs
@@ -86,14 +119,19 @@ app.get('/stopLoop', (req, res) => {
 const foreverLoopFunction = async () => { 
   for (let i = 0; foreverLoop; i++) {
     count = i;
-    console.log('i: ' + i);
+    logger.info('write loop: ' + i);
     await service.createUser(mySqlConnection);
   }
 }
 
 const foreverReadFunction = async () => { 
   for (let i = 0; foreverRead; i++) {
-    console.log('read loop: ' + i);
+    logger.info('read loop: ' + i);
     await service.getFullUsers(mySqlConnection);
   }
+}
+
+function logRequest(req, res, next) {
+  logger.info(req.method + ' ' + req.url);
+  next();
 }
--- services/user.js
+++ services/user.js
@@ -1,5 +1,26 @@
 import mysql from 'mysql2/promise';
 import User from '../models/user.js';
+import winston from 'winston';
+
+const { combine, timestamp, label, printf  } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'services/user' }),
+    timestamp(),
+    myFormat
+  ),
+  // defaultMeta: { service: 'index.js' },
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
 
 export default class UserService {
   constructor() {
@@ -23,24 +44,28 @@ export default class UserService {
       const [results] = await mySqlConnection.execute(query);
     } catch (e) {
       console.error('Unable to create user in MySQL. ', e);
+      logger.error('Unable to create user in MySQL. ', e);
     }
     // console.log(results);
+    logger.info(`${username} created.`);
     return user;
   }
 
   async getFullUsers(mySqlConnection) {
     let result = '';
-    let users = await User.find({ $limit: 1000});
-    console.log('users: ' + users.length);
+    let users = await User.find();
+    // console.log('users: ' + users.length);
     result += `MongoDB: ${users.length}. `;
     
-    let query = `select * from \`easy-aiops\`.\`users\` limit 1000`;    
+    let query = `select * from \`easy-aiops\`.\`users\``;    
     try {
       let [results ] = await mySqlConnection.execute(query);
       result += `MySQL: ${results.length}. `;
     } catch (e) {
       console.error('Unable to create user in MySQL. ', e);
+      logger.error('Unable to create user in MySQL. ', e);
     }
+    logger.info(result);
     return result;
   }
 }
